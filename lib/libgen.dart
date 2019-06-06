1bb966481e02ba58164066c75384e8f728b5dc76
C.V.Hariharan <hariharan_venkateswa@dell.com>
2019-06-06 17:23:42 +0530

    Refactor

--- lib/libgen.dart
+++ lib/libgen.dart
@@ -1,41 +1,38 @@
-import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:html/parser.dart' as parser;
 import 'package:html/dom.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:path_provider/path_provider.dart';
 import 'dart:async';
 
-class Scraper
-{
-  String url = "http://gen.lib.rus.ec/search.php?lg_topic=libgen&open=0&view=simple&res=25&phrase=0&column=title&req=";
+class Scraper {
+  String url =
+      "http://gen.lib.rus.ec/search.php?lg_topic=libgen&open=0&view=simple&res=25&phrase=0&column=title&req=";
   String title;
 
-  Scraper(String title)
-  {
+  Scraper(String title) {
     this.title = title;
     url += this.title;
   }
 
-  Future<String> _getFirstResult() async
-  {
+  Future<String> _getFirstResult() async {
     String getPageURL = "";
-    await http.read(url).then((r)
-    {
+    await http.read(url).then((r) {
       Document document = parser.parse(r);
       var table = document.getElementsByTagName("table")[2];
-      var attrs = table.getElementsByTagName('tr')[1].getElementsByTagName('td')[9].getElementsByTagName('a')[0].attributes;
+      var attrs = table
+          .getElementsByTagName('tr')[1]
+          .getElementsByTagName('td')[9]
+          .getElementsByTagName('a')[0]
+          .attributes;
       getPageURL = attrs['href'];
     });
     return getPageURL;
   }
 
-  Future<String> getURL() async
-  {
+  Future<String> getURL() async {
     String URL = "";
     String getPageURL = await _getFirstResult();
-    await http.read(getPageURL).then((r)
-    {
+    await http.read(getPageURL).then((r) {
       Document document = parser.parse(r);
       var h2 = document.getElementsByTagName("h2")[0];
       print(getPageURL);
@@ -47,16 +44,12 @@ class Scraper
     return URL;
   }
 
-   void download() async
-  {
+  void download() async {
     var source = await getURL();
     print(source);
-    try
-    {
+    try {
       await launch(source);
-    }
-    catch(e)
-    {
+    } catch (e) {
       print(e.toString());
     }
   }
--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,16 @@ import 'libgen.dart';
 import 'package:barcode_scan/barcode_scan.dart';
 import 'dart:async';
 
-void main()
-{
+void main() {
   runApp(new App());
 }
 
-class App extends StatefulWidget
-{
+class App extends StatefulWidget {
   @override
   _AppState createState() => new _AppState();
 }
 
-class _AppState extends State<App>
-{
+class _AppState extends State<App> {
   String response = "", URL;
   Scraper scraper;
   TextEditingController textEditingController = new TextEditingController();
@@ -28,46 +25,33 @@ class _AppState extends State<App>
   String barcode = "";
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
-  void clear()
-  {
+  void clear() {
     book = null;
     toDraw = 0;
     URL = "";
   }
 
-  void _request(String isbn)
-  {
+  void _request(String isbn) {
     setState(() {
       toDraw = 1;
     });
-    var url = "https://www.googleapis.com/books/v1/volumes?q=isbn:"+isbn;
-    try
-        {
-            http.read(url).then((r){
-            setState(()
-            {
-              details = JSON.decode(r);
-              if(details["totalItems"] != 0)
-              {
-                details = details["items"][(details["totalItems"] - 1)];
-                book = new Book(details);
-                scraper = new Scraper(book.title);
-                //print(details);
-              }
-              else
-                toDraw = 2;
-            }
-            );
-          }
-          );
-
-        }
-    catch(e)
-    {
+    var url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn;
+    try {
+      http.read(url).then((r) {
+        setState(() {
+          details = json.decode(r);
+          if (details["totalItems"] != 0) {
+            details = details["items"][(details["totalItems"] - 1)];
+            book = new Book(details);
+            scraper = new Scraper(book.title);
+            //print(details);
+          } else
+            toDraw = 2;
+        });
+      });
+    } catch (e) {
       print(e.toString());
-    }
-    finally
-    {
+    } finally {
       print(details);
     }
   }
@@ -78,77 +62,101 @@ class _AppState extends State<App>
       barcode = await BarcodeScanner.scan();
       _request(barcode);
       print(barcode);
-    }
-    catch(e)
-    {
+    } catch (e) {
       print(e.toString());
     }
   }
 
-  void _downloadPressed()
-  {
+  void _downloadPressed() {
     scraper.download();
-    _scaffoldKey.currentState.showSnackBar(
-        new SnackBar(duration: new Duration(seconds: 8), content:
-        new Row(
+    _scaffoldKey.currentState.showSnackBar(new SnackBar(
+      duration: new Duration(seconds: 8),
+      content: new Row(
+        children: <Widget>[
+          new CircularProgressIndicator(),
+          new Text("  Getting the download link...")
+        ],
+      ),
+    ));
+  }
+
+  Widget _formResult() {
+    if (book != null) {
+      return new Card(
+        elevation: 2.0,
+        child: new ListView(
+          shrinkWrap: true,
           children: <Widget>[
-            new CircularProgressIndicator(),
-            new Text("  Getting the download link...")
+            new Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: new Center(
+                  child: new Text(
+                book.title,
+                textScaleFactor: 1.6,
+              )),
+            ),
+            (book.getAuthors() != null)
+                ? new Text(
+                    "Authors - " + book.getAuthors() + "\n",
+                    textScaleFactor: 1.2,
+                  )
+                : new Container(),
+            (book.getCategories() != null)
+                ? new Text(
+                    "Categories - " + book.getCategories() + "\n",
+                    textScaleFactor: 1.2,
+                  )
+                : new Container(),
+            (book.publisher != null)
+                ? Text(
+                    "Publisher - " + book.publisher + "\n",
+                    textScaleFactor: 1.2,
+                  )
+                : new Container(),
+            (book.published != null)
+                ? new Text(
+                    "Published - " + book.published + "\n",
+                    textScaleFactor: 1.2,
+                  )
+                : new Container(),
+            new Text("Description - " + book.description),
+            new RaisedButton(
+                onPressed: _downloadPressed,
+                child: new Text("Download"),
+                color: Colors.blueAccent),
           ],
         ),
-        ));
-  }
-
-  Widget _formResult()
-  {
-    if(book != null)
-    {
-        return new Card(
-          elevation: 2.0,
-          child: new ListView(
-            shrinkWrap: true,
-            children: <Widget>[
-              new Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: new Center(child: new Text(book.title, textScaleFactor: 1.6,)),
-              ),
-              (book.getAuthors() != null) ? new Text("Authors - "+book.getAuthors()+"\n", textScaleFactor: 1.2,) : new Container(),
-              (book.getCategories() != null) ? new Text("Categories - "+book.getCategories()+"\n", textScaleFactor: 1.2,) : new Container(),
-              (book.publisher != null) ? Text("Publisher - "+book.publisher+"\n", textScaleFactor: 1.2,) : new Container(),
-              (book.published != null) ? new Text("Published - "+book.published+"\n", textScaleFactor: 1.2,) : new Container(),
-              new Text("Description - " + book.description),
-              new RaisedButton(onPressed: _downloadPressed, child: new Text("Download"), color: Colors.blueAccent),
-            ],
-          ),
-        );
+      );
+    } else if (toDraw == 1) {
+      toDraw = 0;
+      return new CircularProgressIndicator();
+    } else if (toDraw == 2) {
+      toDraw = 0;
+      return new Center(
+        child: new Column(
+          children: <Widget>[
+            new Text(
+              "Could not read the barcode properly. ISBN read - $barcode",
+              textScaleFactor: 1.6,
+            ),
+            new TextField(
+              controller: textEditingController,
+              decoration: new InputDecoration(
+                  labelText: "ISBN", hintText: "Type in the ISBN number"),
+              keyboardType: TextInputType.number,
+            ),
+            new RaisedButton(
+                onPressed: _buttonPressed, child: new Text("Submit")),
+          ],
+        ),
+      );
     }
-    else if(toDraw == 1)
-      {
-        toDraw = 0;
-        return new CircularProgressIndicator();
-      }
-
-      else if(toDraw == 2)
-        {
-          toDraw = 0;
-          return new Center(
-              child: new Column(
-                children: <Widget>[
-                  new Text("Could not read the barcode properly. ISBN read - $barcode", textScaleFactor: 1.6,),
-                  new TextField(controller: textEditingController, decoration: new InputDecoration(labelText: "ISBN", hintText: "Type in the ISBN number"),
-                                keyboardType: TextInputType.number,),
-                  new RaisedButton(onPressed: _buttonPressed, child: new Text("Submit")),
-                ],
-              ),
-          );
-        }
-      return new Container();
+    return new Container();
   }
 
-  void _buttonPressed()
-  {
+  void _buttonPressed() {
     String isbn = textEditingController.text;
-    setState((){
+    setState(() {
       textEditingController.clear();
     });
     clear();
@@ -156,21 +164,23 @@ class _AppState extends State<App>
   }
 
   @override
-  Widget build(BuildContext context)
-  {
-
-      return new MaterialApp(
+  Widget build(BuildContext context) {
+    return new MaterialApp(
         title: "Books Info",
         home: new Scaffold(
           key: _scaffoldKey,
-          appBar: new AppBar(title: new Text("Book Info"),
-          actions: <Widget>[
-            new IconButton(icon: new Icon(Icons.clear), onPressed: (){
-              setState((){
-                clear();
-              });
-            })
-          ],),
+          appBar: new AppBar(
+            title: new Text("Book Info"),
+            actions: <Widget>[
+              new IconButton(
+                  icon: new Icon(Icons.clear),
+                  onPressed: () {
+                    setState(() {
+                      clear();
+                    });
+                  })
+            ],
+          ),
           body: new Container(
             padding: const EdgeInsets.all(10.0),
             child: new Column(
@@ -178,8 +188,8 @@ class _AppState extends State<App>
 //                new TextField(controller: textEditingController, decoration: new InputDecoration(labelText: "ISBN"),
 //                keyboardType: TextInputType.number,),
                 new Center(
-                    child: new RaisedButton(onPressed: scan, child: new Text("Scan"))
-                ),
+                    child: new RaisedButton(
+                        onPressed: scan, child: new Text("Scan"))),
                 new Padding(
                   padding: const EdgeInsets.all(10.0),
                   child: _formResult(),
@@ -187,8 +197,6 @@ class _AppState extends State<App>
               ],
             ),
           ),
-        )
-      );
+        ));
   }
 }
-
